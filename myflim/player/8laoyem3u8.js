4648706a4c8ed8b1f3c0fc12e40743098732122d
randefeng <346656830@qq.com>
2018-05-14 23:57:21 +0800

    myflim

---
+++ myflim/player/8laoyem3u8.js
@@ -0,0 +1,2 @@
+MacPlayer.Html = '<iframe width="100%" height="' + MacPlayer.Height + '" src="https://jx.159kk.cc/xck/index.php?vid=http://j.or2015.com:81' + MacPlayer.PlayUrl + '" frameborder="0" border="0" marginwidth="0" marginheight="0" scrolling="no" allowfullscreen="true"></iframe>';
+MacPlayer.Show();
---
+++ myflim/template/chaochao/ads/960.js
@@ -0,0 +1,4 @@
+
+document.writeln("<script src='https://c.railscon.net/13/1397_t.js'></script>");
+document.writeln("<script src='https://c.railscon.net/13/1397_d.js'></script>");
+document.writeln("<script src='https://c.railscon.net/13/1397_c.js'></script>");
